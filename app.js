30d59644c979271ea2c8779ddb274b497656e1d6
navin2701 <navinkumar2701007@gmail.com>
2025-12-29 12:04:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
+
+// ✅ Allow frontend domains
+app.use(cors({
+  origin: [
+    "https://navin.cloud",
+    "https://www.navin.cloud"
+  ]
+}));
 
 app.get("/", (req, res) => {
   res.json({
