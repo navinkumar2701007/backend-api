0737e2526e7c60939b18caf4e6004775394deb9e
navin2701 <navinkumar2701007@gmail.com>
2025-12-29 13:07:27 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   res.json({
     status: "success",
-    message: "Backend auto deploy working manually ",
+    message: "backend auto deploy working 123 ",
     service: "backend-api",
     timestamp: new Date().toISOString()
   });
